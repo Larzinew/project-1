f03f0215804fc821df4ab3b526b6954986a2d4f1
larzinew <larzinewright@gmail.com>
2023-11-14 10:16:32 -0800

    js-evt lis

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
-/*----- constants -----*/
-
+	/*----- constants -----*/
+//array of strings - image names 
+const IMAGES = [1,2,3.4]
 
 	/*----- state variables -----*/
-
+let result = [null, null,null]
 
 	/*----- cached elements  -----*/
-
+const reel1 = document.getElementById('reel1')
+const reel2 = document.getElementById('reel2')
+const reel3 = document.getElementById('reel3')
+const win = document.getElementById('win-message')
+const spinButton = document.querySelector('.spin-button')
 
 	/*----- event listeners -----*/
-
+spinButton.addEventListener('click', initSpin)
 
 	/*----- functions -----*/
